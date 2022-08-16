38fc482907fc71f1d3b0b72858899a98f0374922
Phu Duc Nguyen <ndphu.hightech@gmail.com>
2022-08-16 20:06:47 +0700

    Code CreateUserServlet and UserServices Classes

--- BookStoreWebsite/src/main/java/com/bookstore/service/UserServices.java
+++ BookStoreWebsite/src/main/java/com/bookstore/service/UserServices.java
@@ -1,10 +1,15 @@
 package com.bookstore.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.bookstore.dao.UserDAO;
 import com.bookstore.entity.Users;
@@ -21,9 +26,26 @@ public class UserServices {
 		userDAO = new UserDAO(entityManager);
 	}
 	
-	public List<Users> listUser() {
+	public void listUser(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		List<Users> listUsers = userDAO.listAll();
 		
-		return listUsers;
+		request.setAttribute("listUsers", listUsers);
+		request.setAttribute("message", "New user created successfully");
+		
+		String listPage = "user_list.jsp";
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher(listPage);
+		
+		requestDispatcher.forward(request, response);
+	}
+
+	public void createUser(HttpServletRequest request, HttpServletResponse response) {
+		String email = request.getParameter("email");
+		String fullName = request.getParameter("fullname");
+		String password = request.getParameter("password");
+		
+		Users newUser = new Users(email, fullName, password);
+		userDAO.create(newUser);
 	}
 }
--- BookStoreWebsite/src/main/webapp/admin/user_list.jsp
+++ BookStoreWebsite/src/main/webapp/admin/user_list.jsp
@@ -15,6 +15,13 @@
 		<h3><a href="user_form.jsp">Create new User</a></h3>	
 	</div>
 	
+	<!-- TODO: Just show message after user created -->
+	<c:if test="${message != null}">
+	<div align="center">
+		<h4><i>${message}</i></h4>
+	</div>
+	</c:if>
+	
 	<div align="center">
 		<table border="1" cellpadding="5">
 			<tr>
