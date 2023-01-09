679b1a13688a9c96359d307580a85aa4c7ec5768
Phu Duc Nguyen <sillycat@duck.com>
2023-01-09 16:58:15 +0700

    Implement Add book to Cart

--- BookStoreWebsite/src/main/java/com/bookstore/controller/frontend/shoppingcart/AddBookToCart.java
+++ BookStoreWebsite/src/main/java/com/bookstore/controller/frontend/shoppingcart/AddBookToCart.java
@@ -8,26 +8,38 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.bookstore.dao.BookDAO;
 import com.bookstore.entity.Book;
-import static com.bookstore.util.CommonUtility.*;
 
-@WebServlet("/view_cart")
-public class ViewCartServlet extends HttpServlet {  
+@WebServlet("/add_to_cart")
+public class AddBookToCart extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
-  public ViewCartServlet() {
+  public AddBookToCart() {
     super();
   }
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-
+    Integer bookId = Integer.parseInt(request.getParameter("book_id"));
+    
     Object cartObject = request.getSession().getAttribute("cart");
-
-    if (cartObject == null) {
-      ShoppingCart shoppingCart = new ShoppingCart();
+    
+    ShoppingCart shoppingCart = null;
+    
+    if (cartObject != null && cartObject instanceof ShoppingCart) {
+      shoppingCart = (ShoppingCart) cartObject;
+    } else {
+      shoppingCart = new ShoppingCart();
       request.getSession().setAttribute("cart", shoppingCart);
     }
     
-    forwardToPage("frontend/shopping_cart.jsp", request, response);
+    BookDAO bookDAO = new BookDAO();
+    Book book = bookDAO.get(bookId);
+    
+    shoppingCart.addItem(book);
+    
+    // Redirect to cart's list page
+    String cartPage = request.getContextPath().concat("/view_cart");
+    response.sendRedirect(cartPage);
   }
+
 }
